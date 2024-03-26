2bde9136f1e3d9c65938e9aa2aaae2c7361a5179
Bevan Jacinto <bevanjacinto.bj@gmail.com>
2024-03-25 18:09:45 -0700

    Build an API for updating issues

---
+++ emails/WelomeTemplate.tsx
@@ -0,0 +1,35 @@
+import React, { CSSProperties } from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Tailwind,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome Aboard!</Preview>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://www.google.com">www.google.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+const body: CSSProperties = {
+  background: "#fff",
+};
+const heading: CSSProperties = {
+  fontSize: "32px",
+};
+
+export default WelomeTemplate;
